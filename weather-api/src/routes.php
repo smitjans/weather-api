5b1536eaa2628f8265a2ea273aa70c04a92edb14
Simone Mitjans <mitjans.simone@gmail.com>
2019-01-28 19:47:33 +0100

    Implementata lista meteo

--- weather-api/src/routes.php
+++ weather-api/src/routes.php
@@ -5,10 +5,17 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get('/weather', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
 
+    
+
+    $query = $this->db->prepare("SELECT * FROM Weather");
+    $query->execute();
+    $data =  $query->fetchAll();
+
+    return $response->withJson($data);
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    //return $this->renderer->render($response, 'index.phtml', $args);
 });
