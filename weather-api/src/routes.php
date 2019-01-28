8233716cdb46b555b332fd1abf050e7f159fc4e1
Simone Mitjans <mitjans.simone@gmail.com>
2019-01-28 20:51:23 +0100

    Implementato get post delete e update

--- weather-api/src/routes.php
+++ weather-api/src/routes.php
@@ -13,9 +13,126 @@ $app->get('/weather', function (Request $request, Response $response, array $arg
 
     $query = $this->db->prepare("SELECT * FROM Weather");
     $query->execute();
-    $data =  $query->fetchAll();
+    $data =  $query->fetchAll(); //Ritorna una lista di oggetti
 
-    return $response->withJson($data);
+    return $response->withJson($data); //Prendo la lista di oggetti e la converto in un json che ritorno
     // Render index view
     //return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+
+
+
+$app->get('/weather/[{id}]', function (Request $request, Response $response, array $args) {
+
+
+	$id = $args['id'];
+
+    
+
+    $query = $this->db->prepare("SELECT * FROM Weather WHERE id=$id");
+    $query->bindParam('id', $args['id']);
+    $query->execute();
+    $data =  $query->fetchAll(); //Ritorna una lista di oggetti
+
+    return $response->withJson($data); //Prendo la lista di oggetti e la converto in un json che ritorno
+});
+
+
+$app->post('/weather', function (Request $request, Response $response, array $args) {
+
+
+	$id = $args['id'];
+
+	$query = $this->db->prepare("INSERT INTO Weather (weather, date, temperature)
+            VALUES (:weather, :date, :temperature)");
+
+ 		
+ 		$input = $request->getParsedBody();
+        $query->bindParam('weather', $input['weather']);
+        $query->bindParam('date', $input['date']);
+        $query->bindParam('temperature', $input['temperature']);
+
+        $query->execute();
+
+
+		$query = $this->db->prepare("SELECT * FROM Weather");
+		$query->execute();
+        $data =  $query->fetchAll();
+
+
+
+
+        return $response->withJson($data);
+
+
+
+
+    
+
+    $query = $this->db->prepare("SELECT * FROM Weather WHERE id=$id");
+    $query->execute();
+    $data =  $query->fetchAll(); //Ritorna una lista di oggetti
+
+    return $response->withJson($data); //Prendo la lista di oggetti e la converto in un json che ritorno
+});
+
+
+$app->put('/weather/[{id}]', function (Request $request, Response $response, array $args) {
+
+
+	$id = $args['id'];
+
+	$query = $this->db->prepare("UPDATE Weather set weather=:weather, date=:date, temperature=:temperature where id=:id");
+
+ 		
+ 		$input = $request->getParsedBody();
+        $query->bindParam('weather', $input['weather']);
+        $query->bindParam('date', $input['date']);
+        $query->bindParam('temperature', $input['temperature']);
+        $query->bindParam('id', $args['id']);
+
+        $query->execute();
+
+
+		$query = $this->db->prepare("SELECT * FROM Weather");
+		$query->execute();
+        $data =  $query->fetchAll();
+
+
+
+
+        return $response->withJson($data);
+
+
+
+
+    
+
+    $query = $this->db->prepare("SELECT * FROM Weather WHERE id=$id");
+    $query->execute();
+    $data =  $query->fetchAll(); //Ritorna una lista di oggetti
+
+    return $response->withJson($data); //Prendo la lista di oggetti e la converto in un json che ritorno
+});
+
+
+
+$app->delete('/weather/[{id}]', function (Request $request, Response $response, array $args) {
+
+
+	$id = $args['id'];
+
+    
+
+    $query = $this->db->prepare("DELETE FROM Weather WHERE id=$id");
+    $query->execute();
+
+    $query = $this->db->prepare("SELECT * FROM Weather");
+	$query->execute();
+    $data =  $query->fetchAll(); //Ritorna una lista di oggetti
+
+    return $response->withJson($data); //Prendo la lista di oggetti e la converto in un json che ritorno
+});
+
+
